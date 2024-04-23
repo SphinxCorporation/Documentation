87dd3bfacec70a4d563b845459b728aa583988e6
Eshan Roy (Eshanized) <m.eshanized@gmail.com>
2024-04-23 09:21:09 +0530

    [create] eshanized push script for github.

---
+++ eshanized.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Author            : Eshan Roy
+# Author URI        : https://eshanized.github.io
+
+# NOTE: Run this at your own risk! 
+
+# GitHub repository URL
+REPO_URL="https://github.com/SphinxCorporation/Documentation.git"
+
+# Check if there are any changes to commit
+if [[ -n $(git status -s) ]]; then
+    # Add all changes to the staging area
+    git add .
+
+    # Prompt user for commit message
+    read -p "Commit message: " COMMIT_MESSAGE
+
+    # Commit changes with a default message
+    git commit -m "$COMMIT_MESSAGE"
+
+    # Push changes to the remote repository
+    git push $REPO_URL
+
+    # Check if the push was successful
+    if [ $? -eq 0 ]; then
+        echo "Push successful!"
+    else
+        echo "Error: Push failed!"
+    fi
+else
+    echo "No changes to commit."
+fi
